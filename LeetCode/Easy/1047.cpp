eeeb9d2ed31199e46e6d280c4b4f2eb1c0baa5e2
Casper Hsu <caspertt2009@gmail.com>
2020-09-26 22:19:00 -0700

    updating for Bloomberg

---
+++ LeetCode/Easy/1047.cpp
@@ -0,0 +1,14 @@
+/*
+* 1047. Remove All Adjacent Duplicates In String
+*/
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        
+    }
+};
---
+++ LeetCode/Medium/1209.cpp
@@ -0,0 +1,14 @@
+/*
+* 1209. Remove All Adjacent Duplicates in String II
+*/
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        
+    }
+};
