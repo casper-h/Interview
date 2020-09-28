564d5ff885cd738863f52f95f697bb66890f0444
Casper Hsu <caspertt2009@gmail.com>
2020-09-27 22:19:32 -0700

    adding Bloomberg 1D

--- LeetCode/Easy/1047.cpp
+++ LeetCode/Easy/1047.cpp
@@ -9,6 +9,16 @@ using std::string;
 class Solution {
 public:
     string removeDuplicates(string S) {
-        
+        int i = 0;
+        // i = next output char (what we are "looking" at)
+        // j = iteration of input char
+        for (int j = 0; j < S.size(); i++, j++) {
+            S[i] = S[j];
+            // k = 2, can always expand for k = n, "aaa"
+            if (i > 0 && S[i] == S[i - 1]) {
+                i -= 2;
+            }
+        }
+        return S.substr(0, i);
     }
 };
