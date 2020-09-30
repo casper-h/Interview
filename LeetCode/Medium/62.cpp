d4c34e1fcdb6b493917d4de21baa83ad386373e5
Casper Hsu <caspertt2009@gmail.com>
2020-09-30 16:58:45 -0700

    adding more dp

--- LeetCode/Medium/62.cpp
+++ LeetCode/Medium/62.cpp
@@ -2,10 +2,25 @@
 * 62. Unique Paths
 */
 #include <iostream>
+#include <vector>
+
+using std::vector;
 
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    // Only 1 way to reach the first row/col
+                    dp[i][j] = 1;
+                } else {
+                    // You find the "left" and "top"
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
--- LeetCode/Medium/64.cpp
+++ LeetCode/Medium/64.cpp
@@ -30,6 +30,6 @@ public:
                 }
             }
         }
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
     }
 };
