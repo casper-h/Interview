90bd502f9011ff3fab14faa935bc3e5f877a5d6e
Casper Hsu <caspertt2009@gmail.com>
2020-10-05 21:57:08 -0700

    adding common phone interview

--- LeetCode/Medium/380.cpp
+++ LeetCode/Medium/380.cpp
@@ -2,28 +2,51 @@
 * 380. Insert Delete GetRandom O(1)
 */
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstdlib>
+
+using std::vector;
+using std::unordered_map;
 
 class RandomizedSet {
 public:
     /** Initialize your data structure here. */
-    RandomizedSet() {
-        
-    }
+    RandomizedSet() {}
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        
+        if (randomSet.find(val) == randomSet.end()) {
+            arr.push_back(val);
+            randomSet[val] = arr.size() - 1;
+            return true;
+        }
+        return false;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        
+        if (randomSet.find(val) != randomSet.end()) {
+            int last = arr.back();
+            // Swap index of map, as we prepare to overwrite
+            randomSet[last] = randomSet[val];
+            // Overwrite element to be removed with last element
+            arr[randomSet[last]] = last;
+            arr.pop_back();
+            randomSet.erase(val);
+            return true;
+        }
+        return false;
     }
     
     /** Get a random element from the set. */
     int getRandom() {
-        
+        return arr[rand() % arr.size()];
     }
+
+private:
+    vector<int> arr;
+    unordered_map<int, int> randomSet;
 };
 
 /**
